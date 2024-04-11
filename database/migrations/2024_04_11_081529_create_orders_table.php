ed3741d140ec31791452d1d65f09adf04387e1d1
aliefdany <aliefseventri@gmail.com>
2024-04-11 16:04:40 +0700

    added columns: console_available_id (fk), user_id (fk), and status for orders schema

--- database/migrations/2024_04_11_081529_create_orders_table.php
+++ database/migrations/2024_04_11_081529_create_orders_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('console_available_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->enum('status', ['AVAILABLE', 'ORDERED']);
         });
     }
 
