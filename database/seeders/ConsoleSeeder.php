4789c7d85f827caa990d109dfeed563ecac45f9f
aliefdany <aliefseventri@gmail.com>
2024-04-11 17:04:08 +0700

    updated console seeder to seed few consoles

--- database/seeders/ConsoleSeeder.php
+++ database/seeders/ConsoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Console;
 
 class ConsoleSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class ConsoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $consoles = array('Playstation 5', 'Xbox One', 'PC');
+
+        foreach ($consoles as $console) {
+            $consoleMod = new Console;
+            $consoleMod->name = $console;
+            $consoleMod->save();
+        }     
     }
 }
