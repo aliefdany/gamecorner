15fdf969ee0686eaa869825ef9114c5e8ee28145
aliefdany <aliefseventri@gmail.com>
2024-04-11 15:37:42 +0700

    added columns: code, status, and user_id (with fk to console id)

--- database/migrations/2024_04_11_081435_create_console_availables_table.php
+++ database/migrations/2024_04_11_081435_create_console_availables_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('console_availables', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained();
+            $table->string('code');
+            $table->string('status');
         });
     }
 
